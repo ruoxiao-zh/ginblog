2be49002a9013b914786119107e201b4cdd416b0
zhangruoxiao <zhangruoxiao@doumi.com>
2021-08-04 14:57:20 +0800

    add route and model

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"net/http"
+	"time"
+
+	"ginblog/internal/routers"
+)
+
+func main() {
+	router := routers.NewRouter()
+	s := &http.Server{
+		Addr:           ":8080",
+		Handler:        router,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	s.ListenAndServe()
+}
