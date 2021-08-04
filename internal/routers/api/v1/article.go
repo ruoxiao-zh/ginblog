a6ff89342cf4912da2f73e4252f84b34cecbfa50
zhangruoxiao <zhangruoxiao@doumi.com>
2021-08-04 16:53:27 +0800

    add response

--- internal/routers/api/v1/article.go
+++ internal/routers/api/v1/article.go
@@ -1,6 +1,10 @@
 package v1
 
-import "github.com/gin-gonic/gin"
+import (
+	"ginblog/pkg/app"
+	"ginblog/pkg/errcode"
+	"github.com/gin-gonic/gin"
+)
 
 type Article struct{}
 
@@ -9,7 +13,8 @@ func NewArticle() Article {
 }
 
 func (a Article) Get(c *gin.Context) {
-
+	app.NewResponse(c).ToErrorResponse(errcode.ServerError)
+	return
 }
 
 func (a Article) List(c *gin.Context) {
